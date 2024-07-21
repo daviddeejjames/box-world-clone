f9f8b7225d5e254af07a05f2f5120c3ba74eeb6d
david-james <david.james@rea-group.com>
2024-07-21 23:34:20 +1000

    Complete first level!

---
+++ scripts/goal.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+var occupied = false
+
+func _on_body_entered(body):
+	if body.is_in_group('box'):
+		print("goal entered")
+		occupied = true
+
+func _on_body_exited(body):
+	
+	if body.is_in_group('box'):
+		print("goal exited")
+		occupied = false
