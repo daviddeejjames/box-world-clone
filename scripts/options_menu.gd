f2664be9c2f812eb88323ec74e289d6d245aa896
daviddeejjames <davidj28827@gmail.com>
2024-08-05 13:09:34 +1000

    Add main and options menus

---
+++ scripts/options_menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _on_music_pressed():
+	pass # Replace with function body.
+
+
+func _on_sfx_pressed():
+	pass # Replace with function body
+	
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
