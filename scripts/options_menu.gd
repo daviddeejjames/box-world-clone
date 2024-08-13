5d866f384dc25145933f9fe329d048034f834cfd
daviddeejjames <davidj28827@gmail.com>
2024-08-13 22:57:24 +1000

    Fix corruption

--- scripts/options_menu.gd
+++ scripts/options_menu.gd
@@ -8,4 +8,4 @@ func _on_sfx_pressed():
 	pass # Replace with function body
 	
 func _on_back_pressed():
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+	get_tree().change_scene_to_file("res://scenes//menus/main_menu.tscn")
