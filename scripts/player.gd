addd942431374b18f539650691eb3d94e4a0bf6b
daviddeejjames <davidj28827@gmail.com>
2024-09-02 23:15:43 +1000

    Update player sprites and movement animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -13,20 +13,21 @@ var inputs = {
 }
 
 # Read user input
-func _input(event):
+func _input(event) -> void:
 	for direction in inputs.keys():
 		if Input.is_action_pressed(direction):
+			_animate_direction(direction)
 			move(direction)
 
 # Does the object in the next position have collision?
-func isNextPositionValid(nextPosition):
+func _is_next_position_valid(nextPosition: Vector2) -> bool:
 	ray.target_position = nextPosition
 	ray.force_raycast_update()
 	return !ray.is_colliding()
 
-func move(direction):
+func move(direction) -> void:
 	var nextPosition = inputs[direction] * gridSize
-	if isNextPositionValid(nextPosition):
+	if _is_next_position_valid(nextPosition):
 		self.position += nextPosition 
 	else: 
 		var collider = ray.get_collider()
@@ -40,3 +41,19 @@ func move(direction):
 				}
 				lastPlayerPos.push_back(undoDict)
 				self.position += nextPosition
+
+# Animates player sprite depending on direction
+func _animate_direction(direction: String) -> void:
+	var sprite = $AnimatedSprite2D
+	
+	match direction:
+		'ui_up':
+			sprite.set_animation('backwards')
+		'ui_down':
+			sprite.set_animation('default')
+		'ui_left':
+			sprite.flip_h = true
+			sprite.set_animation('pushing')
+		'ui_right':
+			sprite.flip_h = false
+			sprite.set_animation('pushing')
